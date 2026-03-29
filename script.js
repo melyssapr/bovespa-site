13fb4bc0eb7b72ce3f324032a84da69c63fef60a
Melyssa Pereira <meelyssa2411@gmail.com>
2026-03-28 22:41:55 -0300

    Ajuste geral no projeto: HTML, CSS

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function scrollToSection() {
     behavior: "smooth"
   });
 }
+
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      el.classList.add("visible");
+    }
+  });
+});
